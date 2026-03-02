f2e1ec86aa46ce62aaf540e0c38ba9f50ae9547e
alan <danrotech99@gmail.com>
2026-03-02 18:44:20 +0800

    模擬可以鍵盤遙控車子

--- nav.sh
+++ nav.sh
@@ -4,3 +4,9 @@ ros2 launch slam_toolbox online_async_launch.py slam_params_file:=config/mapper_
 ros2 launch nav2_bringup navigation_launch.py params_file:=/home/alanros/ros2_diffbot_jazzy/src/example_2/bringup/config/nav2_params.yaml
 ros2 run twist_mux twist_mux --ros-args --params-file ./src/example_2/bringup/config/twist_mux.yaml -r cmd_vel_out:=cmd_vel -p use_stamped:=true
 ros2 launch teleop_twist_joy teleop-launch.py joy_config:='xbox' publish_stamped_twist:=True joy_vel:=cmd_vel_joy
+
+
+
+
+
+ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diff_cont/cmd_vel_unstamped -p stamped:=true
---
+++ src/example_2/bringup/launch/diffbot_sim.launch.py
@@ -0,0 +1,221 @@
+# Copyright 2020 ros2_control Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ament_index_python import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+import os
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gui",
+            default_value="true",
+            description="Start RViz2 automatically with this launch file.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_mock_hardware",
+            default_value="false",
+            description="Start robot with mock hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="false",
+            description="Use simulation time.",
+        )
+    )
+
+    # Initialize Arguments
+    gui = LaunchConfiguration("gui")
+    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    
+    default_world = os.path.join(
+        get_package_share_directory('ros2_control_demo_example_2'), 
+        'worlds', 
+        'my_world.sdf')
+    
+    world = LaunchConfiguration('world')
+    
+    world_arg = DeclareLaunchArgument(
+        name='world',
+        default_value=default_world,
+        description='SDF world file'
+    )
+    
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+            launch_arguments={
+                'gz_args': ['-r -v4 ', world],'on_exit_shutdown': 'true'
+            }.items()
+        )
+    
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', '/robot_description',
+                   '-entity', 'diffdrive_robot',
+                   '-z', '1.0'],
+        output='screen',
+    )
+    
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("ros2_control_demo_example_2"), "urdf", "diffbot.urdf.xacro"]
+            ),
+            " ",
+            "use_mock_hardware:=",
+            use_mock_hardware,
+            " ",
+            "use_sim_time:=",
+            use_sim_time,
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("ros2_control_demo_example_2"),
+            "config",
+            "diffbot_controllers.yaml",
+        ]
+    )
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("ros2_control_demo_description"), "diffbot/rviz", "diffbot.rviz"]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        output="both",
+    )
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(gui),
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+    
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "diffbot_base_controller",
+            "--param-file",
+            robot_controllers,
+            "--controller-ros-args",
+            "-r /diffbot_base_controller/cmd_vel:=/cmd_vel",
+            "--controller-ros-args",
+            "-r /diffbot_base_controller/odom:=/odom",
+        ],
+    )
+
+    bridge_params = os.path.join(get_package_share_directory('ros2_control_demo_example_2'), 'config', 'gz_bridge.yaml')
+    
+    ros_gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}'],
+        output='screen',
+    )
+    
+    # ros_gz_bridge = Node(
+    #     package="ros_gz_bridge",
+    #     executable="parameter_bridge",
+    #     arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+    #     parameters=[{
+    #         "qos_overrides./tf_static.publisher.durability": "transient_local"
+    #     }],
+    #     output="screen",
+    # )
+    # Delay rviz start after `joint_state_broadcaster`
+    # delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=joint_state_broadcaster_spawner,
+    #         on_exit=[rviz_node],
+    #     )
+    # )
+
+    # Delay start of joint_state_broadcaster after `robot_controller`
+    # TODO(anyone): This is a workaround for flaky tests. Remove when fixed.
+    # delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=robot_controller_spawner,
+    #         on_exit=[joint_state_broadcaster_spawner],
+    #     )
+    # )
+
+    # nodes = [
+    #     world_arg,
+    #     gz_sim,
+    #     spawn_entity,
+    #     # control_node,
+    #     robot_state_pub_node,
+    #     # robot_controller_spawner,
+    #     # delay_rviz_after_joint_state_broadcaster_spawner,
+    #     delay_joint_state_broadcaster_after_robot_controller_spawner,
+    #     # ros_gz_bridge,
+    # ]
+    
+    nodes = [
+        robot_state_pub_node,
+        world_arg,
+        gz_sim,
+        spawn_entity,
+        joint_state_broadcaster_spawner,
+        robot_controller_spawner,
+        ros_gz_bridge,
+        # control_node,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes)
